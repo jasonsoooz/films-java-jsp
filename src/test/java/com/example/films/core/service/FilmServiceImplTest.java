6b49054f9b7510038943fa3e863742d51da8639e
Jason Soo <jasonsoooz@yahoo.com>
2018-12-23 14:02:30 +1100

    Remove junit 5 spring extension annotation thats not needed

--- src/test/java/com/example/films/core/service/FilmServiceImplTest.java
+++ src/test/java/com/example/films/core/service/FilmServiceImplTest.java
@@ -4,16 +4,13 @@ import com.example.films.port.dto.FilmDTO;
 import com.example.films.port.provides.FilmService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class FilmServiceImplTest {
 
